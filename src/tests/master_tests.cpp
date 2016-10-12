23db2f8d0f0d972c8f68518f2b6e2a2d8cb2a039
Zhitao Li <zhitaoli.cs@gmail.com>
2016-10-11 17:16:05 -0700

    Populated `recovered_slaves` in `/state` and `/slaves` endpoints.
    Bugs closed: MESOS-6177
    
    Reviewed at https://reviews.apache.org/r/52765/

--- src/tests/master_tests.cpp
+++ src/tests/master_tests.cpp
@@ -1825,7 +1825,8 @@ TEST_F(MasterTest, SlavesEndpointWithoutSlaves)
 
   Try<JSON::Value> expected = JSON::parse(
       "{"
-      "  \"slaves\" : []"
+      "  \"slaves\" : [],"
+      "  \"recovered_slaves\" : []"
       "}");
 
   ASSERT_SOME(expected);
